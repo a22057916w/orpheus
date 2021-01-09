44d1e85a122d8194d004ef061513a28e798e99a4
a22057916w <a22057916w@gmail.com>
2021-01-09 23:01:47 +0800

    Create a function used to grab all track info from IDs

--- test.py
+++ test.py
@@ -17,8 +17,36 @@ def getTrackIDs(user, playlist_id):
         ids.append(track['id'])
     return ids
 
-ids = getTrackIDs('angelicadietzel', '4R0BZVh27NUJhHGLNitU08')
+
+def getTrackFeatures(id):
+  meta = sp.track(id)
+  features = sp.audio_features(id)
+
+  # meta
+  name = meta['name']
+  album = meta['album']['name']
+  artist = meta['album']['artists'][0]['name']
+  release_date = meta['album']['release_date']
+  length = meta['duration_ms']
+  popularity = meta['popularity']
+
+  # features
+  acousticness = features[0]['acousticness']
+  danceability = features[0]['danceability']
+  energy = features[0]['energy']
+  instrumentalness = features[0]['instrumentalness']
+  liveness = features[0]['liveness']
+  loudness = features[0]['loudness']
+  speechiness = features[0]['speechiness']
+  tempo = features[0]['tempo']
+  time_signature = features[0]['time_signature']
+
+  track = [name, album, artist, release_date, length, popularity, danceability, acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, time_signature]
+  return track
 
 if __name__ == "__main__":
+
+    ids = getTrackIDs('angelicadietzel', '4R0BZVh27NUJhHGLNitU08')
+
     print(len(ids))
     print(ids)
