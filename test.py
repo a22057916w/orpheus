220cabbb116db41f67464e5a1a4d6910735cc5d2
a22057916w <a22057916w@gmail.com>
2021-01-09 22:40:21 +0800

    Retrieve IDs for each track for a user named as paras_1

--- test.py
+++ test.py
@@ -8,3 +8,13 @@ client_secret = '5aaaae91f4264ed9871d4fd38a1ad8f6'
 
 client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def getTrackIDs(user, playlist_id):
+    ids = []
+    playlist = sp.user_playlist(user, playlist_id)
+    for item in playlist['tracks']['items']:
+        track = item['track']
+        ids.append(track['id'])
+    return ids
+
+ids = getTrackIDs('angelicadietzel', '4R0BZVh27NUJhHGLNitU08')
