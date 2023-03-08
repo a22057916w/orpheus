f524636032527a458363f3e9c30ae3aa33c3b9f1
naitea9527 <a22057916w@gmail.com>
2023-03-08 21:44:10 +0800

    fix packet loss disconnect

--- cogs/music.py
+++ cogs/music.py
@@ -25,7 +25,8 @@ ytdl_format_options = {
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 ffmpeg_options = {
-    'options': '-vn'
+    'options': '-vn',
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"   # packet loss handler
 }
 
 # 建立音樂下載器
