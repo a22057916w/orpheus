5ac73209b7a697f5462158eb223f573f170954eb
naitea9527 <a22057916w@gmail.com>
2023-03-11 16:23:58 +0800

    cls MusicPlayer v1.01

--- cogs/music.py
+++ cogs/music.py
@@ -59,9 +59,44 @@ class YTDLSource(discord.PCMVolumeTransformer):
 class MusicPlayer:
     def __init__(self, ctx):
         self.bot = ctx.Bot
-
         self.queue = asyncio.Queue()
         self.next = asyncio.Event()
+
+        ctx.bot.loop.create_task(self.player_loop())
+
+    async def player_loop(self):
+        """ main player loop """
+
+        # Waits until the client’s internal cache is all ready.
+        await self.bot.wait_until_ready()
+
+        while not self.bot.is_closed():
+            self.next.clear()
+
+            try:
+                # Wait for the next song. If we timeout cancel the player and disconnect...
+                async with timeout(300):    # 5 minutes...
+                    source = await self.queue.get()
+            except asyncio.TimeoutError:
+                await ctx.voice_client.disconnect()
+
+            if not isinstance(source, YTDLSource):
+                # Source was probably a stream (not downloaded)
+                # So we should regather to prevent stream expiration
+                try:
+                    source = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+                except Exception as e:
+                    await ctx.send(f'There was an error processing your song.\n'
+                                             f'```css\n[{e}]\n```')
+                    continue
+
+                ctx.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
+                await ctx.send('Now playing: {}'.format(source.title))
+
+                await self.next.wait()
+
+                # Make sure the FFmpeg process is cleaned up.
+                source.cleanup()
 
 class Music(commands.Cog):
     def __init__(self, bot):
