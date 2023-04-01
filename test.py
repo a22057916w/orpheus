37119638111ebc6204d78c3c773fc31dc0acfed8
naitea9527 <a22057916w@gmail.com>
2023-04-01 17:03:48 +0800

    selenium webdriver

--- test.py
+++ test.py
@@ -1,24 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 
-html = """
-<div class="class1 class2">
-    <p>hello world 1</p>
-</div>
-<div class="class1">
-    <p>hello world 2</p>
-</div>
+# 設定 Chrome 瀏覽器的選項
+options = webdriver.ChromeOptions()
+options.add_argument("--headless") # 不開啟瀏覽器視窗
 
-<div class="class1 class2">
-    <p>hello world 3</p>
+# 建立 Chrome 瀏覽器物件
+driver = webdriver.Chrome(options=options)
 
-"""
+url = "https://www.youtube.com/playlist?list=PL1urwPG3M3NKahUbWDj_Y1qgwtRvhMpN7"
+# 前往要爬取的網頁
+driver.get(url)
 
-soup = BeautifulSoup(html, "html.parser")
+# Render the dynamic content to static HTML
+html = driver.page_source
+# print(html)
 
-# 使用 select 方法來搜尋有 class1 與 class2 兩個 class 的 div 標籤
-div_tags = soup.select(".class1.class2")
+with open("html.txt", "w", encoding="utf-8") as f:
+    f.write(html)
 
-# 取得每個 div 標籤內的文字
-for div_tag in div_tags:
-    text = div_tag.text
-    print(text)
+# Parse the static HTML
+soup = BeautifulSoup(html, 'html.parser')
+
+# 找出所有影片連結所在的元素 by css selector
+video_links = soup.select('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer')
+
+# 印出每個影片的 URL
+for link in video_links:
+    video_url = 'https://www.youtube.com' + link['href']
+    print(video_url)
