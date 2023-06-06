11cb91c000a3d36900610b4b8de556388b21bb4a
naitea9527 <a22057916w@gmail.com>
2023-06-06 12:16:32 +0800

    add is_playlist(), get_playlist()

--- scripts/youtube.py
+++ scripts/youtube.py
@@ -107,3 +107,47 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, to_run)
 
         return cls(discord.FFmpegPCMAudio(data['url'], **ffmpeg_options), data=data, requester=requester)
+
+
+    def is_playlist(self, url):
+        if self.url_source == _YOUTUBE_:
+            match = re.search("list=\w+", url)
+            return False if not match else True
+
+    def get_playlist(self, url):
+        try:
+            # 設定 Chrome 瀏覽器的選項
+            options = webdriver.ChromeOptions()
+            options.add_argument("--headless") # 不開啟瀏覽器視窗
+
+            # 建立 Chrome 瀏覽器物件
+            driver = webdriver.Chrome(options=options)
+
+            # https://www.youtube.com/watch?v=ouLndhBRL4w&list=PL1urwPG3M3NKahUbWDj_Y1qgwtRvhMpN7&index=6
+            url = "https://www.youtube.com/playlist?" + url.split("&")[1]
+            print(url)
+
+            # 前往要爬取的網頁
+            driver.get(url)
+
+            # Render the dynamic content to static HTML
+            html = driver.page_source
+            # print(html)
+
+            with open("html.txt", "w", encoding="utf-8") as f:
+                f.write(html)
+
+            # Parse the static HTML
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # 找出所有影片連結所在的元素 by css selector
+            video_links = soup.select('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer')
+
+            video_url = []
+            # 印出每個影片的 URL
+            for link in video_links:
+                video_url.append('https://www.youtube.com' + link['href'])
+            return video_url
+        except Exception as e:
+            printLog("[get_playlist][E] Unexcepted Error : %s" % e)
+            return None
