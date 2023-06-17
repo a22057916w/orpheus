21b34e3743eb4114885d9c57dfb5fecfe6d7fede
naitea9527 <a22057916w@gmail.com>
2023-06-18 01:52:38 +0800

    Listens for when a track ends

--- cogs/loop_queue.py
+++ cogs/loop_queue.py
@@ -12,6 +12,36 @@ class LoopQ(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.GenericTrack, reason):
+        """
+        Listens for when a track ends.
+        """
+        ctx = player.ctx
+        vc = player.guild.voice_client
+
+        # If the track is on loop, play it again.
+        if vc.loop:
+            return await play_utils.play_track(ctx, vc, track)
+
+        # Add the song to history
+        queue_utils.add_to_previous_queue(track)
+
+        # If the queue is not empty, play the next song.
+        if not vc.queue.is_empty:
+            next_song = vc.queue.get()
+            await play_utils.play_track(ctx, vc, next_song)
+
+        elif vc.loopq:
+            vc.queue = config.LOOPQ.copy()
+            next_song = vc.queue.get()
+            await play_utils.play_track(ctx, vc, next_song)
+
+        # If the queue is empty, stop the player.
+        else:
+            await vc.stop()
+            await ctx.send('**Queue has concluded.**')
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(LoopQ(bot))
