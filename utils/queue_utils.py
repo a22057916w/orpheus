c507b9f0e97d550c1e5d75cf8f275875ba1285f2
naitea9527 <a22057916w@gmail.com>
2023-06-18 01:51:27 +0800

    add push() and pop()

--- utils/queue_utils.py
+++ utils/queue_utils.py
@@ -1,3 +1,16 @@
 import random
 from config import PREVIOUS_TRACKS, LOOPQ
 import wavelink
+
+
+def add_to_previous_queue(track: wavelink.GenericTrack):
+    PREVIOUS_TRACKS.append(track)
+    if len(PREVIOUS_TRACKS) > 10:
+        PREVIOUS_TRACKS.pop(0)
+
+
+def get_from_previous_tracks(pos: int) -> wavelink.GenericTrack:
+    try:
+        return PREVIOUS_TRACKS.pop(pos)
+    except IndexError:
+        return None
