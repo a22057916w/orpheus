489af2937c38c4019932fb11463fcacfbf0aae81
naitea9527 <a22057916w@gmail.com>
2023-06-18 01:55:29 +0800

    add """Loops the current song.""" commands

--- cogs/loop_queue.py
+++ cogs/loop_queue.py
@@ -42,7 +42,6 @@ class LoopQ(commands.Cog):
             await vc.stop()
             await ctx.send('**Queue has concluded.**')
 
-
     @commands.command(aliases=['lq'])
     async def loopq(self, ctx: commands.Context):
         """Loops the queue."""
@@ -67,6 +66,26 @@ class LoopQ(commands.Cog):
             config.LOOPQ.put_at_front(current_track)
             await ctx.send('**Queue is now on loop :repeat:**')
 
+    @commands.command(aliases=['l'])
+    async def loop(self, ctx: commands.Context):
+        """Loops the current song."""
+        vc = await play_utils.get_voice_client(ctx)
+        if not vc:
+            return
+
+        # If the bot is not playing anything, return.
+        if not vc.is_playing:
+            return await ctx.send('I am not playing anything.')
+
+        # If the song is on loop, turn it off.
+        if vc.loop:
+            vc.loop = False
+            await ctx.send('**Loop disabled**')
+        # If the song is not on loop, turn it on.
+        else:
+            vc.loop = True
+            current_track = play_utils.get_currenly_playing(vc)
+            await ctx.send(f'**Looping {current_track.title}:repeat:**')
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(LoopQ(bot))
