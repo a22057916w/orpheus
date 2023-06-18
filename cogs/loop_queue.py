c3ae2175a34d0f3a2fcf1d9dd7ca76603d4c02c5
naitea9527 <a22057916w@gmail.com>
2023-06-19 00:53:36 +0800

    restore on_wavelink_track_end(), get ctx

--- cogs/loop_queue.py
+++ cogs/loop_queue.py
@@ -12,39 +12,37 @@ class LoopQ(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    # @commands.Cog.listener()
-    # async def on_wavelink_track_end(self, payload: wavelink.TrackEventPayload):
-    #     """
-    #     Listens for when a track ends.
-    #     """
-    #     player = payload.player
-    #     track = payload.track
-    #
-    #     vc = player.guild.voice_client
-    #
-    #     ctx = await self.bot.get_context()
-    #
-    #     # If the track is on loop, play it again.
-    #     if vc.loop:
-    #         return await play_utils.play_track(ctx, vc, track)
-    #
-    #     # Add the song to history
-    #     queue_utils.add_to_previous_queue(track)
-    #
-    #     # If the queue is not empty, play the next song.
-    #     if not vc.queue.is_empty:
-    #         next_song = vc.queue.get()
-    #         await play_utils.play_track(ctx, vc, next_song)
-    #
-    #     elif vc.loopq:
-    #         vc.queue = config.LOOPQ.copy()
-    #         next_song = vc.queue.get()
-    #         await play_utils.play_track(ctx, vc, next_song)
-    #
-    #     # If the queue is empty, stop the player.
-    #     else:
-    #         await vc.stop()
-    #         await ctx.send('**Queue has concluded.**')
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, payload: wavelink.TrackEventPayload):
+        """
+        Listens for when a track ends.
+        """
+        player = payload.player
+        track = payload.track
+
+        vc = player.guild.voice_client
+        ctx = player.ctx
+        # If the track is on loop, play it again.
+        if vc.loop:
+            return await play_utils.play_track(ctx, vc, track)
+
+        # Add the song to history
+        queue_utils.add_to_previous_queue(track)
+
+        # If the queue is not empty, play the next song.
+        if not vc.queue.is_empty:
+            next_song = vc.queue.get()
+            await play_utils.play_track(ctx, vc, next_song)
+
+        elif vc.loopq:
+            vc.queue = config.LOOPQ.copy()
+            next_song = vc.queue.get()
+            await play_utils.play_track(ctx, vc, next_song)
+
+        # If the queue is empty, stop the player.
+        else:
+            await vc.stop()
+            await ctx.send('**Queue has concluded.**')
 
     @commands.command(aliases=['lq'])
     async def loopq(self, ctx: commands.Context):
