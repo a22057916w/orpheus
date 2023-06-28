5d937d2e0046b505cc8388b190dd8c575adae361
naitea9527 <a22057916w@gmail.com>
2023-06-28 21:14:01 +0800

    Create embed_utils.py

---
+++ utils/embed_utils.py
@@ -0,0 +1,14 @@
+import discord
+import wavelink
+import datetime
+from config import PREVIOUS_TRACKS, PREFIX
+from discord.ext import commands
+
+
+class EmbedGenerator:
+    """
+    Embeds for the bot.
+    """
+
+    def __init__(self) -> None:
+        pass
