a4af6026c68f2e4fca8c625b9e4ca9172639ba80
naitea9527 <a22057916w@gmail.com>
2026-03-25 23:13:01 +0800

    reset environment

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_CLIENT_SECRET_ID = os.getenv("SPOTIFY_CLIENT_SECRET_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 PREFIX = '!'
 
